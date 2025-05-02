8d3cfd49a2cacf53c5c80062a3d167af8afd4f50
Gabriela Garached <garacheddantas@gmail.com>
2025-05-02 11:26:50 -0300

    Primeiro commit do projeto SpotifeiGD

---
+++ main/SpotifeiGD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.spotifei.main;
+
+/**
+ *
+ * @author garac
+ */
+
+public class SpotifeiGD {
+
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Spotifei!");
+        // chamar a tela de login dps aqui
+    }
+}
